a0049b72e6d7bb0f7475e806b07e7b362922be92
Onyet App <onyetcorp@gmail.com>
2020-02-07 16:34:40 +0700

    Update print.php
    Mengubah nama printer

--- print.php
+++ print.php
@@ -5,13 +5,10 @@ if (isset($_POST['data'])) {
 
     $text   = $_POST['data'];
 
-    /* tulis dan buka koneksi ke printer */    
-    $printer = printer_open("\\OFFICE2-PC\Canon iP2700 series");  
-    /* write the text to the print job */  
+    $printer = printer_open("EPSON TM-U220 Receipt"); 
     printer_write($printer, $text);   
-    /* close the connection */ 
     printer_close($printer);
-
+    
 } else {
     
     echo json_encode(array('data' => array(), 'error' => '505'));
